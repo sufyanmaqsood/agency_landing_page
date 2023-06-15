b75ef64ae3a37a20beeb09d2d34c35dc786bad6d
Sufyan Maqsood <sufyanmaqsood6@gmail.com>
2023-06-15 10:37:07 +0500

    Need improvements

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'theme': '#FD5545',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
